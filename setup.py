6594aca01a581382c9ffbc4a35ed70877a9917e0
Yanshuo CHEN <chen-ys18@mails.tsinghua.edu.cn>
2021-10-11 15:40:18 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'scTAPE',
+    version = '0.1',
+    description = 'deep learning tools for bulk RNA-seq deconvolution and gene expression analysis',
+    author = 'Yanshuo CHEN',
+    author_email = 'poseidonchan@icloud.com',
+    url = 'https://github.com/poseidonchan/TAPE',
+    license = 'GPL-3.0 License',
+    packages = find_packages(),
+    included_package_data = True,
+    platforms = 'any',
+    install_requires = [
+        'torch>=1.8.0',
+        'numpy>=1.19',
+        'pandas>=1.3.1',
+        'matplotlib>=3.4',
+        'anndata==0.7.6',
+        'tqdm>=4.6',
+        'scikit-learn>=0.23',
+    ],
+
+)
