cf945aed0063a7f371b8fd23c564a767432bb32a
Yanshuo CHEN <chen-ys18@mails.tsinghua.edu.cn>
2022-05-31 01:07:22 +0800

    updated

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'scTAPE',
-    version = '1.0.1',
+    version = '1.0.2',
     description = 'deep learning tools for bulk RNA-seq deconvolution and gene expression analysis',
     author = 'Yanshuo Chen',
     author_email = 'poseidonchan@icloud.com',
@@ -12,7 +12,7 @@ setup(
     platforms = 'any',
     install_requires = [
         'torch==1.8.0',
-        'numpy==1.19',
+        'numpy==1.21',
         'pandas==1.3.1',
         'matplotlib==3.4',
         'anndata==0.7.6',
